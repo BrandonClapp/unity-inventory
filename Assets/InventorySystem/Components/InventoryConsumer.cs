16f13e9ed59bb1e7143957cab722333b5ce87732
Brandon Clapp <clapp09@gmail.com>
2016-07-17 15:14:54 -0500

    add temporary normal cursor

--- Assets/InventorySystem/Components/InventoryConsumer.cs
+++ Assets/InventorySystem/Components/InventoryConsumer.cs
@@ -9,48 +9,50 @@ public class InventoryConsumer : MonoBehaviour
     Rigidbody rigidbody;
     Inventory inventory;
     Texture2D lootCursor;
+	Texture2D normalCursor;
 
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
         inventory = GetComponentInChildren<Inventory>();
-        lootCursor = Resources.Load<Texture2D>("Cursors/lootcursor");
+		normalCursor = Resources.Load<Texture2D>("Cursors/normal-cursor");
+        lootCursor = Resources.Load<Texture2D>("Cursors/loot-cursor");
+
+		Cursor.SetCursor (normalCursor, new Vector2 (0, 0), CursorMode.Auto);
     }
 
     void Update()
     {
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
-        {
-            if (hit.collider.tag == "Loot")
-            {
-                if (Input.GetButtonDown("Fire2"))
-                {
-                    // check how close our character is to the loot
-                    //Vector3.Distance(hit.collider.t)
-                    var playerPosition = rigidbody.position;
-                    var hitPosition = hit.collider.transform.position;
-                    var distance = Vector3.Distance(playerPosition, hitPosition);
+		if (Physics.Raycast (ray, out hit)) {
+			if (hit.collider.tag == "Loot") {
+				if (Input.GetButtonDown ("Fire2")) {
+					// check how close our character is to the loot
+					//Vector3.Distance(hit.collider.t)
+					var playerPosition = rigidbody.position;
+					var hitPosition = hit.collider.transform.position;
+					var distance = Vector3.Distance (playerPosition, hitPosition);
 
-                    if (distance < 1.5f)
-                    {
-                        var lootInfo = hit.collider.GetComponent<ItemInfo>();
-                        inventory.AddItem(lootInfo.ItemId);
-                        Destroy(hit.collider.gameObject);
-                    }
-                }
+					if (distance < 1.5f) {
+						var lootInfo = hit.collider.GetComponent<ItemInfo> ();
+						inventory.AddItem (lootInfo.ItemId);
+						Destroy (hit.collider.gameObject);
+					}
+				}
 
-                Debug.Log("lootCursor" + lootCursor);
-                if(lootCursor == null)
-                {
-                    Debug.Log("null");
-                }
+				Debug.Log ("lootCursor" + lootCursor);
+				if (lootCursor == null) {
+					Debug.Log ("null");
+				}
 
-                Cursor.SetCursor(lootCursor, new Vector2(0, 0), CursorMode.Auto);
-            }
-        }
-
+				Cursor.SetCursor (lootCursor, new Vector2 (0, 0), CursorMode.Auto);
+			} 
+			else
+			{
+				Cursor.SetCursor (normalCursor, new Vector2 (0, 0), CursorMode.Auto);
+			}
+		} 
         
 
         // todo: if hovering over loot item, change mouse cursor.
